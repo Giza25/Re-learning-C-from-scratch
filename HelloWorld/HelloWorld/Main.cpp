0af947c9fafdcfe99f75ca82936dcfe41e61f1ef
Giza_5 <dyma125@gmail.com>
2023-07-21 14:54:17 +0400

    #Connected all files #Played with linker and compiler settings in between

--- HelloWorld/HelloWorld/Main.cpp
+++ HelloWorld/HelloWorld/Main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 void Log(const char* message);
+int Multiply(int a, int b);
+
 
 int main()
 {
 	Log("Hello World!");
+	std::cout << Multiply(7, 8) << std::endl;
 	std::cin.get();
 }
